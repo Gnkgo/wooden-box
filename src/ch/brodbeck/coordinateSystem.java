a2ecd8c6925584c347ef5b7bc46c03926732a27e
joannabrodbeck <joanna.brodbeck@hotmail.ch>
2021-03-09 22:31:08 +0100

    for loop instead if

--- src/ch/brodbeck/coordinateSystem.java
+++ src/ch/brodbeck/coordinateSystem.java
@@ -1,13 +1,12 @@
 package ch.brodbeck;
 
+import java.util.Arrays;
+
 public class coordinateSystem {
     public int[][][] coordinate = new int[51][36][26];
     public int xNumberl;
     public int yNumberl;
     public int zNumberl;
-    public int xNumberr;
-    public int yNumberr;
-    public int zNumberr;
     /*
     height = z
     length = y
@@ -38,35 +37,35 @@ public class coordinateSystem {
     }
 
     public boolean checkCollision(int givenX, int givenY, int givenZ, int givenJ, int x, int y, int z, int j) {
-
-        if ((getLeftCorner(x, y, z)[0] > getLeftCorner(givenX, givenY, givenZ)[0]) &&
-                (getLeftCorner(x, y, z)[0]) < (getRightCorner(givenX, givenY, givenZ, givenJ)[0])) {
-            return false;
-        } else if ((getLeftCorner(x, y, z)[1] > getLeftCorner(givenX, givenY, givenZ)[1]) &&
-                (getLeftCorner(x, y, z)[1]) < (getRightCorner(givenX, givenY, givenZ, givenJ)[1])) {
-            return false;
-        } else if ((getLeftCorner(x, y, z)[2] > getLeftCorner(givenX, givenY, givenZ)[2]) &&
-                (getLeftCorner(x, y, z)[2]) < (getRightCorner(givenX, givenY, givenZ, givenJ)[2])) {
-            return false;
-        }else if ((getRightCorner(x, y, z, j)[0] > getLeftCorner(givenX, givenY, givenZ)[0]) &&
-                (getRightCorner(x, y, z, j)[0]) < (getRightCorner(givenX, givenY, givenZ, givenJ)[0])) {
-            return false;
-        } else if ((getLeftCorner(x, y, z)[1] > getLeftCorner(givenX, givenY, givenZ)[1]) &&
-                (getRightCorner(x, y, z, j)[1]) < (getRightCorner(givenX, givenY, givenZ, givenJ)[1])) {
-            return false;
-        } else if ((getLeftCorner(x, y, z)[2] > getLeftCorner(givenX, givenY, givenZ)[2]) &&
-                (getRightCorner(x, y, z, j)[2]) < (getRightCorner(givenX, givenY, givenZ, givenJ)[2])) {
-            return false;
-        } else {
-            return true;
+        for (int i = 0; i<3; i++ ) {
+            if ((getLeftCorner(x, y, z)[i] > getLeftCorner(givenX, givenY, givenZ)[i]) &&
+                    (getLeftCorner(x, y, z)[i]) < (getRightCorner(givenX, givenY, givenZ, givenJ)[i])) {
+                return false;
+            }
         }
+        for (int k = 0; k<3; k++ ) {
+            if ((getRightCorner(x, y, z, j)[k] > getLeftCorner(givenX, givenY, givenZ)[k]) &&
+                    (getRightCorner(x, y, z, j)[k]) < (getRightCorner(givenX, givenY, givenZ, givenJ)[k])) {
+                return false;
+            }
+        }
+        return true;
         //get collision with corners of the box
     }
 
-    public void placeBox(int x, int y, int z, int j){
-        getRightCorner(x, y, z, j)[0] = xNumberl;
-        checkCollision(x, y, z, j);
-        coordinate [x][y][z]
+
+    public int[][][] placeBox(int x, int y, int z, int j) {
+        //checkCollision(x, y, z, j);
+        for (int[][] row : coordinate) {
+            for (int[] column : row) {
+                Arrays.fill(column, -1);
+            }
+        }
+        return coordinate;
+    }
+
+        //coordinate [x][y][z]
+        /*
         for (int i = 0; i < Boxes[j].getWidth(); i++) {
             coordinate [i][y][z] = -1;
             for (int h = 0; j < Boxes[h].getLength(); h++) {
@@ -75,41 +74,35 @@ public class coordinateSystem {
                 for (int k = 0; k < Boxes[j].getHeight(); k++) {
                     coordinate [x][y][k] = -1;
                 }
+         */
             /*
             exception when array is too short,
             then this combination doesn't work,
             either rotate Box or choose another combination
              */
 
-
-        }
-
-    }
     public int[] getLeftCorner(int x, int y, int z) {
-        xNumberl = x;
-        yNumberl = y;
-        zNumberl = z;
         return new int[]{x, y, z};
     }
 
     public int[] getRightCorner(int x, int y, int z, int j) {
         int[] rightTopCorner = {
                 coordinate
-                        [x + Boxes [j].getWidth()]
-                        [y + Boxes [j].getLength()]
-                        [z + Boxes [j].getHeight()]
+                        [x + Boxes[j].getWidth()]
+                        [y + Boxes[j].getLength()]
+                        [z + Boxes[j].getHeight()]
         };
+        return rightTopCorner;
+    }
         /*
         xNumberr += Boxes [j].getWidth();
         yNumberr += Boxes [j].getLength();
         zNumberr += Boxes [j].getHeight();
          */
-        return rightTopCorner;
-    }//one method? getLeftCorner and getRightCorner? Mapping? Pair?
 
-    public void setPosition(int x, int y, int z) {
-        coordinate [x][y][z] = -1;
-    }
+    //one method? getLeftCorner and getRightCorner? Mapping? Pair?
+
+
 
     public void rotateLengthWidth(int length, int width) {
         int a = width;
